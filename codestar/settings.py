7b3cd46cb6c240e7ecf7c5e249af1e494b674ff4
satry22 <sara.tryzell@gmail.com>
2022-02-09 12:16:49 +0000

    edit Allowed Hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['saras-coderstar2022.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['saras-codestar2022.herokuapp.com', 'localhost']
 
 
 # Application definition
